604f6b15886afbf90b772d90e69b017cdba1f493
Sugandha <sugcoder12@gmail.com>
2025-02-02 00:10:08 +0530

    Episode 2 : Igniting our app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("div", {id:'heading', class:'headingclass'}, [React.createElement('h1',{},"Om Namah Shivaya"),React.createElement('h2',{},"Jai Shri Krishna")]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
